77ec1bbca2ce3ac9d92e38521585de9d656c62d0
bryantduncan98@gmail.comgit config --global user.email bryantduncan98@gmail.com <bryantduncan98@gmail.com>
2018-01-15 22:13:34 -0700

    alomst done

--- liri.js
+++ liri.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 //importing from key.js
 var keys = require("./key.js");
-console.log(keys.twitter);
 //needed for twitter api 
 var Twitter = require('twitter');
 var T = new Twitter(keys.twitter); 
@@ -11,11 +10,12 @@ var S = new spotify(keys.spotify);
 
 //allows for user input
 var action = (process.argv[2]);
+//for best result input the title like this "the+goonies"
 var title = (process.argv[3]);
 
 if (action === "my-tweets"){
 	var params = {
-		q: 'yay',
+		q: 'duner_guy',
 		count: 20
 	};
 	T.get('search/tweets', params,searchedData);
@@ -27,18 +27,37 @@ if (action === "my-tweets"){
   if (err) {
     return console.log('Error occurred: ' + err);
   }
- 
-	console.log(data); 
+        var song = data.tracks.items;
+        console.log(song);
+        
+        for (i = 0; i < song.length; i++){
+        console.log("Artist: " + song[i].artists[i].name);
+        console.log("Album Title: " + song[i].album.name);
+        console.log("Spotify Link: " + song[i].preview_url);
+        //***title doesnt display right***
+        console.log("Track Title: " + title);
+        } 
 	});
 }else if (action === "movie-this"){
 	// require for the omdb api
 	 var request = require("request");
-	 var queryUrl = "http:www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy"
+	 var queryUrl = "http://www.omdbapi.com/?t="+ title +"&y=&plot=short&apikey=trilogy"
 	console.log(queryUrl);
 	request(queryUrl, function(error, response, body) {
   		// If there were no errors and the response code was 200 
   		if (!error && response.statusCode === 200) {
-    		console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+  			console.log(response);
+    		console.log("Title: " + JSON.parse(body).Title);
+    		console.log("Year Made: " + JSON.parse(body).Year);    		
+    		console.log("The movie's imdb rating is: " + JSON.parse(body).imdbRating);
+    		//***YOU NEED TO FIX THIS*** 
+    		console.log("The movie's Rotten Tomatoes rating is: " + JSON.parse(body).Rating.Source:RottenTomatoes);
+    		console.log("Made In: " + JSON.parse(body).Country);
+			console.log("It was made in these Language(s): " + JSON.parse(body).Language); 
+			console.log("A PLot of the movie: " + JSON.parse(body).Plot);
+    		console.log("Actors in this movie: " + JSON.parse(body).Actors);
+
+
   		}else{
   			console.log("failed");
   		}
@@ -51,8 +70,9 @@ if (action === "my-tweets"){
  		console.log("error");
  		}
  		else {
-	 		console.log("worked");
+ 			//***you need to make this run in terr.
 	 		console.log(data);
+
 		}
 	});
 }	
